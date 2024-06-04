ceba3f09e4d6b949b56f5e6ab7e57831ca14f979
bariqoctfn <bariqputra3@gmail.com>
2024-06-04 23:54:19 +0700

    days 1

---
+++ days 1.py
@@ -0,0 +1,18 @@
+#Memperkenalkan dasar python
+#string
+print("hello world") #output 'hello world' 
+#integer
+a =2
+b =6
+#float
+c = 3.6 
+#boolean
+d = b > a #output true
+#Aritmatika
+e = c + a
+f = a - b
+g = a * c
+print(d) #output 5.6
+print(e) #output -4
+print(f) #output 7.2
+print(g) #output 3.0
