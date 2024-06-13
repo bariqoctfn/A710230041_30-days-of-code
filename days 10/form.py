03b4bbd5de58f75d79e4c9bccdb8fc32e0f3d706
bariqoctfn <bariqputra3@gmail.com>
2024-06-13 22:08:03 +0700

    day 10

---
+++ days 10/form.py
@@ -0,0 +1,35 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(400, 300)
+        self.widget = QtWidgets.QWidget(Dialog)
+        self.widget.setGeometry(QtCore.QRect(70, 80, 176, 126))
+        self.widget.setObjectName("widget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.widget)
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.nameLineEdit = QtWidgets.QLineEdit(self.widget)
+        self.nameLineEdit.setObjectName("nameLineEdit")
+        self.verticalLayout.addWidget(self.nameLineEdit)
+        self.showMessageButton = QtWidgets.QPushButton(self.widget)
+        self.showMessageButton.setObjectName("showMessageButton")
+        self.verticalLayout.addWidget(self.showMessageButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.widget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.verticalLayout.addWidget(self.pushButton_2)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "masukkan nama"))
+        self.showMessageButton.setText(_translate("Dialog", "Halo"))
+        self.pushButton_2.setText(_translate("Dialog", "keluar"))
