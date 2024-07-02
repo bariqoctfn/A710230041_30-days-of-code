eb94b11f3cfa095baba5ae09989513838eb3769a
bariqoctfn <bariqputra3@gmail.com>
2024-07-02 17:48:42 +0700

    day 29

---
+++ days 29/mainorder.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtGui import *
+from orderplus import CafeOrderApp
+from PyQt5.QtWidgets import QApplication
+
+def main():
+    app = QApplication(sys.argv)
+    gui = CafeOrderApp()
+    gui.setWindowTitle("Cafe Order")
+    gui.setWindowIcon(QIcon('icon.jpg'))
+    gui.setGeometry(600,200,600,700)
+    gui.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
